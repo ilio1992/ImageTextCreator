afa52a77a0e51f572bf3cc5f2c9f86bc1cde75d6
ilio1992 <155165573+ilio1992@users.noreply.github.com>
2024-02-17 18:46:06 +0200

    Add files via upload

--- main_final.py
+++ main_final.py
@@ -274,7 +274,7 @@ if input_type == "Excel File":
             st.success("Template images uploaded successfully!")
 
             # Display user inputs
-            output_path = st.text_input("Enter Output Folder Path:", "output_images")
+            output_path = st.text_input("Enter Output Folder Path:", "Desktop\\output_images")
 
             # Get the number of template images
             num_templates = len(template_images)
